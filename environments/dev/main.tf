5a966d305ea90127a684ec893b24dbad88d6aeed
root <root@ip-172-31-37-116.ap-south-1.compute.internal>
2026-07-20 11:13:10 +0000

    Fix availability zones to match ap-south-1 region

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -44,7 +44,7 @@ module "vpc" {
   name_prefix          = local.name_prefix
   cluster_name         = local.cluster_name
   vpc_cidr             = "10.1.0.0/16"
-  availability_zones   = ["us-east-1a", "us-east-1b"]
+  availability_zones   = ["ap-south-1a", "ap-south-1b"]
   public_subnet_cidrs  = ["10.1.1.0/24", "10.1.2.0/24"]
   private_subnet_cidrs = ["10.1.11.0/24", "10.1.12.0/24"]
   enable_nat_gateway   = true
